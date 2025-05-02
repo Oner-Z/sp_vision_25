83c577e2ae1c127d987ca1c18563ac5e30c83775
fishyu <fishyu1201@163.com>
2025-05-02 13:13:42 +0800

    add a few math tools

--- tools/math_tools.hpp
+++ tools/math_tools.hpp
@@ -3,6 +3,7 @@
 
 #include <Eigen/Geometry>
 #include <chrono>
+#include <opencv2/opencv.hpp>
 
 namespace tools
 {
@@ -41,6 +42,82 @@ Eigen::MatrixXd ypd2xyz_jacobian(const Eigen::Vector3d & ypd);
 // 计算时间差a - b，单位：s
 double delta_time(
   const std::chrono::steady_clock::time_point & a, const std::chrono::steady_clock::time_point & b);
+
+/**
+ * @brief 两个二维点间距离
+ * @param[in] pt1
+ * @param[in] pt2
+ * @return double
+ */
+inline double pointPointDistance(const cv::Point2f& pt1, const cv::Point2f& pt2) noexcept {
+  return std::sqrt((pt1.x - pt2.x) * (pt1.x - pt2.x) + (pt1.y - pt2.y) * (pt1.y - pt2.y));
+}
+
+/**
+* @brief 两个三维点间距离
+* @param[in] pt1
+* @param[in] pt2
+* @return double
+*/
+inline double pointPointDistance(const cv::Point3f& pt1, const cv::Point3f& pt2) noexcept {
+  return std::sqrt((pt1.x - pt2.x) * (pt1.x - pt2.x) + (pt1.y - pt2.y) * (pt1.y - pt2.y) +
+                   (pt1.z - pt2.z) * (pt1.z - pt2.z));
+}
+
+/**
+* @brief 点到直线间距离
+* @param[in] ptP
+* @param[in] ptL1
+* @param[in] ptL2
+* @return double
+*/
+inline double pointLineDistance(const cv::Point2f& ptP, const cv::Point2f& ptL1,
+                              const cv::Point2f& ptL2) noexcept {
+  double A = ptL2.y - ptL1.y;
+  double B = ptL1.x - ptL2.x;
+  double C = ptL2.x * ptL1.y - ptL2.y * ptL1.x;
+  double distance = fabs(A * ptP.x + B * ptP.y + C) / sqrt(A * A + B * B);
+  return distance;
+}
+
+/**
+* @brief 点到直线距离
+* @param[in] pt
+* @param[in] line
+* @return double
+*/
+inline double pointLineDistance(const cv::Point2f& pt, const cv::Vec4f& line) {
+  cv::Vec2f line_dir(line[0], line[1]);
+  cv::Point2f line_pt(line[2], line[3]);
+  return cv::norm((pt - line_pt).cross(line_dir));
+}
+
+/**
+* @brief 求解一元二次方程，返回解的数值对，第一项小于第二项
+* @param[in] a
+* @param[in] b
+* @param[in] c
+* @return std::pair<double, double>
+*/
+inline std::pair<double, double> solveQuadraticEquation(double a, double b, double c) {
+  std::pair<double, double> result((-b - sqrt((double)(b * b - 4 * a * c))) / (2 * a),
+                                   (-b + sqrt((double)(b * b - 4 * a * c))) / (2 * a));
+  return result;
+}
+
+/**
+ * @brief 角度转弧度
+ * @param[in] angle
+ * @return double
+ */
+inline double angle2Radian(double angle) noexcept { return angle * CV_PI / 180; }
+
+/**
+ * @brief 弧度转角度
+ * @param[in] radian
+ * @return double
+ */
+inline double radian2Angle(double radian) noexcept { return radian * 180 / CV_PI; }
 
 }  // namespace tools
 
